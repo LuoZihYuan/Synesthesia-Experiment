ff1acf0c415831f10ab030863d811e60251b67eb
LuoZihYuan <fudan28024@gmail.com>
2017-12-29 10:52:10 +0800

    :heavy_plus_sign:chore(node_modules/react-native-vector-icons): add lib 'react-native-vector-icons'

--- android/app/src/main/java/com/synesthesiaexperiment/MainApplication.java
+++ android/app/src/main/java/com/synesthesiaexperiment/MainApplication.java
@@ -3,13 +3,15 @@ package com.synesthesiaexperiment;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.cmcewen.blurview.BlurViewPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.polidea.reactnativeble.BlePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import com.cmcewen.blurview.BlurViewPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.polidea.reactnativeble.BlePackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,9 +28,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new BlurViewPackage(),
-            new RNDeviceInfo(),
-            new BlePackage()
+          new BlurViewPackage(),
+          new RNDeviceInfo(),
+          new BlePackage(),
+          new VectorIconsPackage()
       );
     }
 
